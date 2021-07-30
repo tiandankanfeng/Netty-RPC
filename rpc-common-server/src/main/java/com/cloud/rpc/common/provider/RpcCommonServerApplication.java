22e11fb4a9d9aa8551cc31f52055e55f6d6de172
tiandankanfeng <2252578955@qq.com>
2021-07-30 17:47:30 +0800

    不建议去使用基于 Nacos的注册中心

--- rpc-common-server/src/main/java/com/cloud/rpc/common/provider/RpcCommonServerApplication.java
+++ rpc-common-server/src/main/java/com/cloud/rpc/common/provider/RpcCommonServerApplication.java
@@ -1,15 +1,19 @@
 package com.cloud.rpc.common.provider;
 
+import com.alibaba.nacos.spring.context.annotation.discovery.EnableNacosDiscovery;
 import com.cloud.rpc.common.provider.server.NettyRpcServer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * CommandLineRunner - 用于应用程序初始化后, 执行一段有逻辑的代码, 而这块初始化代码在整个应用程序生命周期中也只会被执行一次
  */
+//@EnableDiscoveryClient
 @SpringBootApplication
+//@EnableNacosDiscovery
 public class RpcCommonServerApplication implements CommandLineRunner {
 
     @Autowired
---
+++ rpc-common-server/src/main/java/com/cloud/rpc/common/provider/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.cloud.rpc.common.provider.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class HelloController {
+
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello world";
+    }
+}
